5499b10c7a98634b1d57b650e6247e242c69d7a4
yangboz <YoungWelle@gmail.com>
2015-04-15 10:11:19 +0800

    ADDED:C++Tools from XCode6 for Godpaper

---
+++ CPPTools4Godpaper/CPPTools4Godpaper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPPTools4Godpaper
+//
+//  Created by yangboz on 15-4-15.
+//  Copyright (c) 2015年 GODPAPER. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
